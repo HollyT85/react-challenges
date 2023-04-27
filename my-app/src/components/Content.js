de10ce6f537b03f96d1eeb53080fe021957ffa98
HollyT85 <h.e.turner@gmail.com>
2023-04-27 19:29:59 +0000

    post items

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import css from "./css/Content.module.css"
-import {savedPosts} from posts.json
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
 
-function Content() {
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
 
-    return (
-    <div>
-        <div className={css.TitleBar}>
-            <h1>My Photos</h1>
-        </div>
-        <div className={css.SearchResults}>
-            <p>Content Goes Here</p>
-        </div>
-    </div>
-    )
+                <div className={css.SearchResults}>
+                    <PostItem savedPosts={savedPosts} />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Content
